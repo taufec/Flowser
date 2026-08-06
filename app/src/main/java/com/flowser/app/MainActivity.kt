6102d66e1bf87cd27503c0c996a7272716d231a3
taufec <152962399+taufec@users.noreply.github.com>
2026-08-06 12:59:51 +0800

    Add launcher button to start floating browser

--- app/src/main/java/com/flowser/app/MainActivity.kt
+++ app/src/main/java/com/flowser/app/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.flowser.app
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.provider.Settings
+import android.widget.Button
+import android.widget.LinearLayout
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val webView = WebView(this)
-        webView.settings.javaScriptEnabled = true
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://example.com")
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.VERTICAL
 
-        setContentView(webView)
+        val button = Button(this)
+        button.text = "Open Flowser Floating Browser"
+        button.setOnClickListener {
+            if (!Settings.canDrawOverlays(this)) {
+                startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName")))
+            } else {
+                startService(Intent(this, FloatingService::class.java))
+            }
+        }
+
+        layout.addView(button)
+        setContentView(layout)
     }
 }
